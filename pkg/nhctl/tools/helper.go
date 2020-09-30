064946c42e0397b5d5f09a4e2544d80b0f204b3a
huangxinxin <huangxinxin@coding.net>
2020-09-30 11:29:10 +0800

    feat(nhctl): add port-forward command

---
+++ pkg/nhctl/tools/helper.go
@@ -0,0 +1,225 @@
+package sys
+
+import (
+	"go.uber.org/zap"
+	zaplog "coding-deploy-cli/libs/log"
+	"runtime"
+	"os/exec"
+	"encoding/json"
+	"net"
+	"os"
+	"io"
+	"bufio"
+	"fmt"
+	"errors"
+	"strings"
+)
+
+var logger *zap.Logger
+
+//init
+func init() {
+    logger = zaplog.InitLogger()
+}
+
+//check os
+func CheckOS() string{
+	sysType := runtime.GOOS
+	logger.Info("OS name", zap.String("type", sysType))
+	switch(sysType) {
+	case "linux":
+	case "windows":
+	case "darwin":
+		break
+	default:
+		sysType = "unknown"
+		break
+	}
+	return sysType
+}
+
+//check kubernetes version
+func CheckK8s() (string,bool){
+	osName := CheckOS()
+    if osName == "unknown" {
+        panic("nonsupport os system. support linux, macos, windows.")
+    }
+    //kubectl command
+    kubectl := osName+"/kubectl"
+    if osName == "windows" {
+    	kubectl = osName+"/kubectl.exe"
+    }
+
+    ///home/coding-cli/
+    //exec command
+    command := exec.Command("tools/"+kubectl, "version", "-o", "json")
+    versionJson, err := command.CombinedOutput()
+    if err != nil {
+        fmt.Println("cmd.Run() failed with %s\n", err)
+    }
+    var k8sVersion interface{}
+	errVersion := json.Unmarshal(versionJson, &k8sVersion)
+	if errVersion != nil {
+		logger.Error("Kubernetes api-server is not connected or Kubernetes is not installed", zap.Error(errVersion))
+		fmt.Println("error: Kubernetes api-server is not connected or Kubernetes is not installed")
+		return "",false
+	}
+
+	logger.Info("Kubernetes", zap.String("version", string(versionJson)))
+	version := k8sVersion.(map[string]interface{})["serverVersion"].(map[string]interface{})["gitVersion"].(string)
+    return version, true
+}
+
+
+//check ip
+func CheckIp(ip string) {
+    address := net.ParseIP(ip)
+    if address == nil {
+         fmt.Println("ip 地址格式不正确")
+         os.Exit(0)
+     }
+}
+
+
+//check node type
+func CheckNode(nodeType string) {
+	nodeTypes := map[string]string{"worker":"slave node", "master":"master node"}
+    if _, err:= nodeTypes[nodeType] ; !err {
+    	fmt.Println("node type 不正确, 请选择 master 或者 worker")
+    	os.Exit(0)
+    }
+}
+
+
+//execute command
+func ExecCommand(commandName string, params ...string) error{
+	//debug mode
+	execCommand := commandName
+	basePath := "tools/"
+	execCommand = basePath + commandName
+	// check command 
+	if CheckCommand(commandName) {
+		if !CheckFile(execCommand) {
+			return errors.New("error: 执行文件不存在 !")
+		}
+	}
+	cmd := exec.Command(execCommand, params...)
+	fmt.Println(cmd.Args)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("error: 命令执行错误 !")
+	}
+	//start
+	cmd.Start()
+	reader := bufio.NewReader(stdout)
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Print(line+"\n")
+	}
+	cmd.Wait()
+	return nil
+}
+
+
+//Check Command
+func CheckCommand(commandName string) bool{
+	status := false
+	toolsList := []string{
+		"kubectl", 
+		"kubectl.exe", 
+		"mutagen", 
+		"install-k8s-master", 
+		"install-k8s-worker", 
+		"install-k8s-create-kubeconfig", 
+		"coding-deploy",
+		"debug-service",
+		"file-sync",
+		"deploy-no-dependencies"}
+
+	for _, v := range toolsList {
+		if strings.Contains(commandName, v)	{
+			status = true
+			break
+		}
+	}
+	return status
+}
+
+
+//file exist check
+func CheckFile(file string) bool{
+	_, err := os.Stat(file)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+
+//check cmd exist
+func CheckCmdExists(cmd string) bool{
+    path, err := exec.LookPath(cmd)
+    if err != nil {
+        logger.Error("check command is not exists ", zap.Error(err))
+        return false
+    } else {
+        logger.Info("check command is exists ", zap.String("command", cmd), zap.String("path", path))
+        return true
+    }
+}
+
+
+//check namespace
+func CheckNS(namespace string) error{
+	osName := CheckOS()
+    if osName == "unknown" {
+        panic("nonsupport os system. support linux, macos, windows.")
+    }
+    //kubectl command
+    kubectl := osName+"/kubectl"
+    if osName == "windows" {
+    	kubectl = osName+"/kubectl.exe"
+    }
+    //exec command
+	command := exec.Command("tools/"+kubectl, "get", "ns", "-o", "json")
+    namespacesJson, err := command.CombinedOutput()
+    if err != nil {
+        fmt.Println("cmd.Run() failed with %s\n", err)
+    }
+    var namespaces interface{}
+	errns := json.Unmarshal(namespacesJson, &namespaces)
+	if errns != nil {
+		logger.Error("get namespace failed", zap.Error(errns))
+		fmt.Println("error: get namespace failed")
+		return errors.New("get namespace failed")
+	}
+
+	logger.Info("Kubernetes", zap.String("namespaces", string(namespacesJson)))
+
+	for _, v := range (namespaces.(map[string]interface{})["items"]).([]interface{}) {
+		metadata := v.(map[string]interface{})["metadata"]
+		ns := metadata.(map[string]interface{})["name"]
+		if namespace == ns.(string) {
+			logger.Error("namespace  exists ", zap.String("namespace", namespace))
+			return errors.New("error: 此用户创建的namespace 已存在, 请更换用户名!")
+		}
+	}
+
+    return nil
+}
+
+
+
+
+
+
+
+
+
+
+
