6a3a8eb324b86eef604a5b71acf5caddd898677a
huangxinxin <huangxinxin@coding.net>
2020-10-09 11:05:37 +0800

    feat(nhctl): update file-sync command

--- pkg/nhctl/cmd/file-sync.go
+++ pkg/nhctl/cmd/file-sync.go
@@ -3,13 +3,16 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"nocalhost/pkg/nhctl/third_party/mutagen"
 )
+
+var localFolderName, remoteAddress string
 
 func init() {
 	//install k8s
 	//fileSyncCmd.Flags().StringVarP(&sessionName, "session", "s", "", "sync session")
-	//fileSyncCmd.Flags().StringVarP(&localFolderName, "local folder", "l", "", "local folder path")
-	//fileSyncCmd.Flags().StringVarP(&remoteAddress, "remote address", "r", "", "remote ip address and folder path")
+	fileSyncCmd.Flags().StringVarP(&localFolderName, "local-folder", "l", "", "local folder path")
+	fileSyncCmd.Flags().StringVarP(&remoteAddress, "remote-address", "r", "", "remote ip address and folder path")
 	//fileSyncCmd.Flags().StringVarP(&remoteAddress, "ssh passwd", "p", "", "ssh passwd")
 	rootCmd.AddCommand(fileSyncCmd)
 }
@@ -19,7 +22,17 @@ var fileSyncCmd = &cobra.Command{
 	Short: "Sync files to remote Pod in Kubernetes",
 	Long: `Sync files to remote Pod in Kubernetes`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if localFolderName == "" {
+			fmt.Println("error: please use -l to specify a local directory to sync with remote")
+			return
+		}
+		if remoteAddress == "" {
+			fmt.Println("error: please use -r to specify a remote address")
+			return
+		}
 		//TO-DO
-		fmt.Println("file syncing...")
+		fmt.Println("file syncing...")  // tools/darwin/mutagen sync create --sync-mode=one-way-safe --name=$1  $2  $3
+											// ./tools/script/file-sync.sh coding dir01 root@127.0.0.1:12345:/home/code
+		mutagen.FileSync(localFolderName)
 	},
 }
--- pkg/nhctl/cmd/port-forward.go
+++ pkg/nhctl/cmd/port-forward.go
@@ -1,14 +1,21 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"github.com/spf13/cobra"
+	"nocalhost/pkg/nhctl/third_party/kubectl"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-var localPort string
+var deployment,localPort,remotePort string
 
 func init() {
-	portForwardCmd.Flags().StringVarP(&localPort, "local", "p", "10000", "local port to forward")
+	portForwardCmd.Flags().StringVarP(&localPort, "local", "l", "10000", "local port to forward")
+	portForwardCmd.Flags().StringVarP(&remotePort, "remote-port", "r", "22", "remote port to be forwarded")
+	portForwardCmd.Flags().StringVarP(&deployment, "deployment", "d", "", "the k8s deployment we want to forward to")
 	rootCmd.AddCommand(portForwardCmd)
 }
 
@@ -17,7 +24,21 @@ var portForwardCmd = &cobra.Command{
 	Short: "Forward local port to remote pod'port",
 	Long: `Forward local port to remote pod'port`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//TO-DO
-		fmt.Printf("port forwarding...local port : %s\n", localPort)
+		if deployment == "" {
+			fmt.Println("error: deployment can not be empty")
+			return
+		}
+		// todo local port should be specificed ?
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGHUP)  // kill -1
+		ctx, cancel := context.WithCancel(context.TODO())
+
+		go func() {
+			<-c
+			cancel()
+			fmt.Println("stop port forward")
+		}()
+
+		kubectl.PortForward(ctx , deployment, localPort, remotePort) // eg : ./utils/darwin/kubectl port-forward --address 0.0.0.0 deployment/coding  12345:22
 	},
 }
