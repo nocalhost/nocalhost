bcf4e373e2fdb7a7cfc6e8cdf563e2a868a6b455
huangxinxin <huangxinxin@coding.net>
2020-10-12 11:50:58 +0800

    feat(nhctl): [debug command] scale deployment's replicas to 1

--- pkg/nhctl/cmd/debug.go
+++ pkg/nhctl/cmd/debug.go
@@ -1,39 +1,74 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"github.com/spf13/cobra"
-	"nocalhost/pkg/nhctl/third_party/mutagen"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-var localFolderName, remoteFolder, sshPort string
+//var localFolderName, remoteFolder, sshPort string
+var nameSpace string
 
 func init() {
 	//install k8s
-	//fileSyncCmd.Flags().StringVarP(&sessionName, "session", "s", "", "sync session")
-	fileSyncCmd.Flags().StringVarP(&localFolderName, "local-folder", "l", "", "local folder path")
-	fileSyncCmd.Flags().StringVarP(&remoteFolder, "remote-folder", "r", "/home/code", "remote folder path")
-	fileSyncCmd.Flags().StringVarP(&sshPort, "port", "p", "22", "ssh port")
+	debugCmd.Flags().StringVarP(&nameSpace, "namespace", "n", "", "kubernetes namespace")
+	debugCmd.Flags().StringVarP(&deployment, "deployment", "d", "", "k8s deployment which you want to forward to")
+
+	//fileSyncCmd.Flags().StringVarP(&localFolderName, "local-folder", "l", "", "local folder path")
+	//fileSyncCmd.Flags().StringVarP(&remoteFolder, "remote-folder", "r", "/home/code", "remote folder path")
+	//fileSyncCmd.Flags().StringVarP(&sshPort, "port", "p", "22", "ssh port")
 	//fileSyncCmd.Flags().StringVarP(&remoteFolder, "ssh passwd", "p", "", "ssh passwd")
-	rootCmd.AddCommand(fileSyncCmd)
+	rootCmd.AddCommand(debugCmd)
 }
 
-var fileSyncCmd = &cobra.Command{
-	Use:   "sync",
-	Short: "Sync files to remote Pod in Kubernetes",
-	Long: `Sync files to remote Pod in Kubernetes`,
+var debugCmd = &cobra.Command{
+	Use:   "debug",
+	Short: "enter debug model",
+	Long: `enter debug model`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if localFolderName == "" {
-			fmt.Println("error: please use -l to specify a local directory to sync with remote")
+		if nameSpace == "" {
+			fmt.Println("error: please use -n to specify a kubernetes namespace")
 			return
 		}
-		if remoteFolder == "" {
-			fmt.Println("error: please use -r to specify a remote folder")
-			return
-		}
+		//if remoteFolder == "" {
+		//	fmt.Println("error: please use -r to specify a remote folder")
+		//	return
+		//}
 		//TO-DO
-		fmt.Println("file syncing...")  // tools/darwin/mutagen sync create --sync-mode=one-way-safe --name=$1  $2  $3
-											// ./tools/script/file-sync.sh coding dir01 root@127.0.0.1:12345:/home/code
-		mutagen.FileSync(localFolderName, remoteFolder, sshPort)
+		fmt.Println("enter debug...")
+		ReplaceImage(nameSpace, deployment)
 	},
+}
+
+func ReplaceImage(nameSpace string, deployment string)  {
+	k8sConfig, err := clientcmd.BuildConfigFromFlags("", "/Users/xinxinhuang/.kube/config")
+	if err != nil {
+		fmt.Printf("%v",err)
+		return
+	}
+
+	clientSet, err := kubernetes.NewForConfig(k8sConfig)
+	if err != nil {
+		fmt.Printf("%v",err)
+		return
+	}
+
+	//dep, err := clientSet.AppsV1().Deployments(nameSpace).Get(context.TODO(), deployment, metav1.GetOptions{})
+
+	scale, err := clientSet.AppsV1().Deployments(nameSpace).GetScale(context.TODO(), deployment, metav1.GetOptions{})
+	if err != nil {
+		fmt.Printf("%v",err)
+		return
+	}
+
+	fmt.Println("debugging deployment: " + deployment)
+	fmt.Sprintf("scaling replicas to 1")
+	if scale.Spec.Replicas > 1 {
+		fmt.Printf("deployment %s's replicas is %d now", deployment, scale.Spec.Replicas)
+	} else {
+		fmt.Printf("deployment %s's replicas is 1\n", deployment)
+	}
 }
