248602cf1e6c0b8bd7eaa2d1e025bb3467c37900
huangxinxin <huangxinxin@coding.net>
2020-10-12 14:58:40 +0800

    feat(nhctl): [port-forward] add error handle
    Former-commit-id: 34647e1c9803296ad627d22f3779cb222b18d284

--- pkg/nhctl/third_party/kubectl/port-forward.go
+++ pkg/nhctl/third_party/kubectl/port-forward.go
@@ -3,19 +3,20 @@ package kubectl
 import (
 	"context"
 	"fmt"
+	"github.com/pkg/errors"
 	"nocalhost/pkg/nhctl/tools"
 )
 
 
 // should be run in the background ??
-func PortForward(ctx context.Context, deployment string , localPort string, remotePort string, args ...string)  {
+func PortForward(ctx context.Context, deployment string , localPort string, remotePort string, args ...string) error {
 	fmt.Println("kubectl port-forwarding...")
 	_, active := tools.CheckK8s()
 	if !active {
 		fmt.Println("kubernetes cluster is unavailable")
-		return
+		return errors.New("kubernetes cluster is unavailable")
 	}
-	tools.ExecKubeCtlCommand(ctx,"port-forward", "--address", "0.0.0.0", fmt.Sprintf("deployment/%s", deployment), fmt.Sprintf("%s:%s",localPort,remotePort))
+	return tools.ExecKubeCtlCommand(ctx,"port-forward", "--address", "0.0.0.0", fmt.Sprintf("deployment/%s", deployment), fmt.Sprintf("%s:%s",localPort,remotePort))
 }
 
 func StopPortForward(){
