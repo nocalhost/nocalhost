15dee5f11562b4a36926170d73a295205d6196fa
王炜 <wangweimax@coding.net>
2020-11-11 19:47:05 +0800

    feat(nocalhost-api): add init cluster and edit user api
    Former-commit-id: 1b0ea20c33ab630556befa953e2cb8d863a4213a

---
+++ internal/nocalhost-api/global/static.go
@@ -0,0 +1,13 @@
+package global
+
+const (
+	NocalhostSystemNamespace              = "nocalhost-reserved"
+	NocalhostSystemNamespaceLabel         = "nocalhost-reserved"
+	NocalhostDevNamespaceLabel            = "nocalhost"
+	NocalhostDevServiceAccountName        = "nocalhost-dev-account"
+	NocalhostDevRoleName                  = "nocalhost-dev-role"
+	NocalhostDevRoleBindingName           = "nocalhost-role-biding"
+	NocalhostDevServiceAccountSecretCaKey = "ca.crt"
+	NocalhostDepKubeConfigMapName         = "nocalhost-kubeconfig"
+	NocalhostDepKubeConfigMapKey          = "config"
+)
