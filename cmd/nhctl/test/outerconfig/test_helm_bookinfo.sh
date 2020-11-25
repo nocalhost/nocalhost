ab07c13c4bb2e6c6d0a325310916102f182006e1
huangxinxin <huangxinxin@coding.net>
2020-11-25 12:21:45 +0800

    feat(nhctl): add helm outerconfig test case
    Former-commit-id: 5c5405e7418da09ff2520aed1a26bdd9fdb4beba

---
+++ cmd/nhctl/test/outerconfig/test_helm_bookinfo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+APPNAME=test-helm-bookinfo-outer-config-01
+kubectl delete ns $APPNAME >> /dev/null
+kubectl create ns $APPNAME
+nhctl uninstall $APPNAME >> /dev/null
+nhctl install $APPNAME -u https://e.coding.net/codingcorp/nocalhost/mini-bookinfo-noconfig.git --debug -n $APPNAME --config helm_config.yaml
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+sleep 3
+nhctl dev start $APPNAME -d details
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+nhctl port-forward $APPNAME -d details &
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+sleep 3
+
+nhctl sync $APPNAME -d details
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+
+sleep 3
+
+nhctl dev end $APPNAME -d  details
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+
+nhctl uninstall $APPNAME
+if [ "$?" != 0 ]; then
+    echo "fail"
+    exit 1
+fi
+kubectl delete ns $APPNAME
+echo "succeed"
