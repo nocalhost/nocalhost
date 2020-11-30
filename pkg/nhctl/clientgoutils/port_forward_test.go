61010cd8341ee42720a17e2c7b1f57463de236fd
huangxinxin <huangxinxin@coding.net>
2020-11-30 10:32:54 +0800

    feat(nhctl): clientgoutil forward

---
+++ pkg/nhctl/clientgoutils/port_forward_test.go
@@ -0,0 +1,43 @@
+package clientgoutils
+
+import (
+	"fmt"
+	"nocalhost/internal/nhctl/utils"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestClientGoUtils_PortForward(t *testing.T) {
+	//utils.GetHomePath()
+	client, err := NewClientGoUtils(filepath.Join(utils.GetHomePath(), ".kube", "admin-config"), time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
+	fps := []*ForwardPort{{LocalPort: 11223, RemotePort: 22}}
+
+	pf, err := client.CreatePortForwarder("demo30", "details-77cc4f49fd-nl7kn", fps)
+	if err != nil {
+		panic(err)
+	}
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- pf.ForwardPorts()
+	}()
+
+	for {
+		select {
+		case <-pf.Ready:
+			fmt.Println("port forward is ready")
+		case <-errChan:
+			fmt.Println("err")
+		}
+
+	}
+
+	time.Sleep(time.Minute)
+	pf.Close()
+
+}
