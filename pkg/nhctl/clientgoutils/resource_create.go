913f3f0492d4dc7dc89c99512ee6b262ef51adb0
黄鑫鑫 <huangxinxin@coding.net>
2020-12-03 20:58:54 +0800

    Accept Merge Request #397: (nhctl-improve-raw-manifest-speed -> release) Merge Request: improve raw manifest install speed Created By: @黄鑫鑫 Accepted By: @黄鑫鑫 URL: https://codingcorp.coding.net/p/nocalhost/d/nocalhost/git/merge/397 Former-commit-id: 391f892a416a856c2214fbf070dd5c6730d9e277

--- pkg/nhctl/clientgoutils/resource_create.go
+++ pkg/nhctl/clientgoutils/resource_create.go
@@ -1,12 +1,69 @@
 package clientgoutils
 
-//import "k8s.io/cli-runtime/pkg/resource"
+import (
+	"errors"
+	"fmt"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/resource"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+)
 
-func (c *ClientGoUtils) CreateResource() {
-	// todo
-	//var builder *resource.Builder
-	//helper := resource.NewHelper(c.GetRestClient(), info.Mapping).
-	//	DryRun(o.DryRunStrategy == cmdutil.DryRunServer).
-	//	WithFieldManager(o.FieldManager)
+func (c *ClientGoUtils) ApplyForCreate(files []string, namespace string, continueOnError bool) error {
+	if len(files) == 0 {
+		return errors.New("files must not be nil")
+	}
+	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+	matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubeConfigFlags)
+	f := cmdutil.NewFactory(matchVersionKubeConfigFlags)
+	builder := f.NewBuilder()
+	//ns, _ := c.GetDefaultNamespace()
+	validate, err := f.Validator(true)
+	if err != nil {
+		return err
+	}
+	filenames := resource.FilenameOptions{
+		Filenames: files,
+		Kustomize: "",
+		Recursive: false,
+	}
+	if continueOnError {
+		builder.ContinueOnError()
+	}
+	result := builder.Unstructured().
+		Schema(validate).
+		NamespaceParam(namespace).DefaultNamespace().
+		FilenameParam(true, &filenames).
+		//LabelSelectorParam(o.Selector).
+		Flatten().Do()
 
+	if result == nil {
+		return errors.New("result is nil")
+	}
+	if result.Err() != nil {
+		return result.Err()
+	}
+
+	infos, err := result.Infos()
+	if err != nil {
+		return err
+	}
+
+	if len(infos) == 0 {
+		return errors.New("no result info")
+	}
+
+	for _, info := range infos {
+		helper := resource.NewHelper(info.Client, info.Mapping)
+		obj, err := helper.Create(info.Namespace, true, info.Object)
+		if err != nil {
+			if continueOnError {
+				continue
+			}
+			return err
+		}
+		info.Refresh(obj, true)
+		fmt.Printf("%s/%s created\n", info.Object.GetObjectKind().GroupVersionKind().Kind, info.Name)
+	}
+
+	return nil
 }
