2ca0939f84cda4f8bc33319bfd7a35328b0558df
王炜 <wangweimax@coding.net>
2020-12-04 21:41:36 +0800

    feat(nocalhost-api): add cluster ready status and not ready message
    Former-commit-id: 435288feb48af94fb3e513bef94f507be8098d92

--- internal/nocalhost-api/repository/cluster/cluster_repo.go
+++ internal/nocalhost-api/repository/cluster/cluster_repo.go
@@ -63,7 +63,7 @@ func (repo *clusterBaseRepo) GetAny(ctx context.Context, where map[string]interf
 
 func (repo *clusterBaseRepo) GetList(ctx context.Context) ([]*model.ClusterList, error) {
 	var result []*model.ClusterList
-	repo.db.Raw("select c.id,c.name,c.info,c.user_id,c.created_at,count(distinct cu.id) as users_count from clusters as c left join clusters_users as cu on c.id=cu.cluster_id where c.deleted_at is null and cu.deleted_at is null group by c.id").Scan(&result)
+	repo.db.Raw("select c.id,c.kubeconfig,c.name,c.info,c.user_id,c.created_at,count(distinct cu.id) as users_count from clusters as c left join clusters_users as cu on c.id=cu.cluster_id where c.deleted_at is null and cu.deleted_at is null group by c.id").Scan(&result)
 	return result, nil
 }
 
