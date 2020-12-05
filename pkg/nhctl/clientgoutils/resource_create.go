314f3087eef697ee17ba460cb21ecdf3755f301e
王炜 <wangweimax@coding.net>
2020-12-05 12:05:57 +0800

    feat(nhctl): apply use

--- pkg/nhctl/clientgoutils/resource_create.go
+++ pkg/nhctl/clientgoutils/resource_create.go
@@ -15,6 +15,7 @@ func (c *ClientGoUtils) ApplyForCreate(files []string, namespace string, continu
 		return errors.New("files must not be nil")
 	}
 	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+	kubeConfigFlags.KubeConfig = &c.kubeConfigFilePath
 	matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubeConfigFlags)
 	f := cmdutil.NewFactory(matchVersionKubeConfigFlags)
 	builder := f.NewBuilder()
