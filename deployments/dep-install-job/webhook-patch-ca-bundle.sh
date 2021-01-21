78f8a2603e6e30ee25b87dadc233b4e60eabbac9
王炜 <wangweimax@coding.net>
2021-01-21 19:46:53 +0800

    feat(nocalhost-dep): in-cluster serviceaccount

--- deployments/dep-install-job/webhook-patch-ca-bundle.sh
+++ deployments/dep-install-job/webhook-patch-ca-bundle.sh
@@ -4,11 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
-
-if [ -z "${CA_BUNDLE}" ]; then
-    CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
-fi
+CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='nocalhost-admin-service-account-token')].data.ca\.crt}")
 
 export CA_BUNDLE
 
