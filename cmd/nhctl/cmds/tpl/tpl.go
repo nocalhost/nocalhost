aa9b7c81ece5336e50409e6baf5230e0f4f1e9a6
xinxinh <huangxinxin@coding.net>
2021-02-08 18:17:47 +0800

    feat(nhctl): update v2 template

--- cmd/nhctl/cmds/tpl/tpl.go
+++ cmd/nhctl/cmds/tpl/tpl.go
@@ -20,174 +20,221 @@ import (
 	"github.com/pkg/errors"
 )
 
-var applicationTpl = `# application name
-# type: string(dns1123)
-# default value: null
-# required
-# uniq
-name: nocalhost
+var applicationTpl = `
+configProperties:
+  version: v2  # config.yaml version
+  envFile: env.dev
 
-# appplication resource type
-# type: select，options：helmGit/helmRepo/rawManifest/kustomize
-# default value: null
-# required
-manifestType: helmGit 
-
-# Manifest resources path(relative to the root directory)
-# type: string[]
-# default value: ["."]
-# required
-resourcePath: ["deployments/chart"]
-
-# run job before install application
-# type: object[]
-# default value: []
-# optional
-onPreInstall:
-  # type: string
+application:
+  # application name
+  # type: string(dns1123)
   # default value: null
   # required
-  - path: "job-1.yaml"
-    # type: integer
-    # default value: 0
-    # optional
-    priority: -1
-  - path: "job-2.yaml"
-    priority: 5
+  # uniq
+  name: coding-agile
 
+  # appplication resource type
+  # type: select，options：helmGit/helmRepo/rawManifest/kustomize
+  # default value: null
+  # required
+  manifestType: rawManifest
+
+  # Manifest resources path(relative to the root directory)
+  # type: string[]
+  # default value: ["."]
+  # required
+  resourcePath: ["deployments/chart"]
+  ignoredPath: []
+
+  # run job before install application
+  # type: object[]
+  # default value: []
+  # optional
+  onPreInstall:
+  	# type: string
+  	# default value: null
+  	# required
+  	- path: "job-1.yaml"
+      # type: integer
+      # default value: 0
+      # optional
+      priority: -1
+  	- path: "job-2.yaml"
+      priority: 5
+
+
+  # Values for helm
+  helmValues:
+    - key: DOMAIN
+      value: ${DOMAIN:-www.coding.com}
+    - key: DEBUG
+      value: ${DEBUG:-true}
+  env: 
+    - name: DEBUG
+      value: ${DEBUG:-true}
+    - name: DOMAIN
+      value: "www.coding.com"
+  envFrom: 
+    envFile: 
+      - path: dev.env
+      - path: dev.env
+  
 `
 
-var svcTpl = `# service name
-# type: string
-# required
-name: {{ . }}
-    
+var svcTpl = `
+
+name: e-coding
+
 # kubernetes workload, such as: deployment,job
 # type: select, options: deployment/statefulset/pod/job/cronjob/daemonset case-insensitive
 # required
 serviceType: deployment
 
-# git url where the source code of this service resides
-# type: string
-# default value: null
-# required
-gitUrl: "https://github.com/nocalhost/nocalhost.git"
+dependLabelSelector: 
+  # pod selectors which service depends on
+  # type: string[]
+  # default value: []
+  # optional
+  # pods: 
+  #   - "name=mariadb"
+  #   - "app.kubernetes.io/name=mariadb"
 
-# develop container image
-# type: string
-# default value: codingcorp-docker.pkg.coding.net/nocalhost/dev-images/golang:latest
-# required
-devContainerImage: "codingcorp-docker.pkg.coding.net/nocalhost/dev-images/golang:latest"
+  # job selectors which service depends on
+  # type: string[]
+  # default value: []
+  # optional
+  # jobs:
+  #   - "job-name=init-job"
+  #   - "app.kubernetes.io/name=init-job"
+containers:
+  - name: xxx 
+    install: 
+      env: 
+        - name: DEBUG
+          value: "true"
+        - name: DOMAIN
+          value: "www.coding.com"
+      envFrom: 
+        envFile: 
+          - path: dev.env
+          - path: dev.env
+      portForward:   # 安装后需要打通的端口
+        - 3306:3306
+    dev:
+	  # git url where the source code of this service resides
+	  # type: string
+	  # default value: null
+	  # required
+      gitUrl: xxx-job
 
-# List of files and directories to be synchronized to DevContainer
-# type: string[]
-# default value: ["."]
-# optional
-# syncFilePattern:
-  # - "./src"
-  # - "./pkg/fff"
+      # develop container image
+      # type: string
+      # default value: codingcorp-docker.pkg.coding.net/nocalhost/dev-images/golang:latest
+      # required
+      image: java:8-jdk
 
-# List of ignored files and directories to be synchronized to DevContainer
-# type: string[]
-# default value: []
-# optional
-# ignoreFilePattern:
-  # - ".git"
-  # - "./build"
+	  # The default shell of DevContainer
+	  # type: string
+	  # default value: "/bin/sh"
+	  # optional
+      # shell: "bash"
 
-# work dir of develop container
-# type: string
-# default value: "/home/nocalhost-dev"
-# optional
-# workDir: "/home/nocalhost-dev"
+	  # work dir of develop container
+	  # type: string
+	  # default value: "/home/nocalhost-dev"
+	  # optional
+      # workDir: "/root/nocalhost-dev"
 
-# Dirs to be persisted in DevContainer
-# type: string[]
-# default value: ["/home/nocalhost-dev"]
-# optional
-# persistentVolumeDirs:
-  # Dir to be persisted in DevContainer
-  # type: string
-  # default value: null
-  # required
-  # - path: "/root"
+	  # resource requirements of dev container
+      # resources:
+      #   limits:
+      #     cpu: "1"
+      #     memory: 1Gi
+      #   requests:
+      #     cpu: "0.5"
+      #     memory: 512Mi
+	  # Dirs to be persisted in DevContainer
+	  # type: string[]
+	  # default value: ["/home/nocalhost-dev"]
+	  # optional
+      # persistentVolumeDirs: 
+		# Dir to be persisted in DevContainer
+  		# type: string
+  		# default value: null
+  		# required
+		# - path: "/root"
+    	# Capability of the dir
+    	# type: string
+    	# default value: 10Gi
+    	# optional
+        #   capacity: 100Gi
+      command: 
+		# Run command of the service
+		# type: string[]
+		# default value: [""]
+		# optional
+        build: ["./gradlew", "package"]
 
-    # Capability of the dir
-    # type: string
-    # default value: 10Gi
-    # optional
-    # capacity: 100Gi
+		# Run command of the service
+		# type: string[]
+		# default value: [""]
+		# optional
+        # run: ["./gradlew", "bootRun"]
 
-# ports which need to be forwarded
-# localPort:remotePort
-# type: string[]
-# default value: []
-# optional
-# devPorts:
-  # - 8080:8080
-  # random localPort, remotePort 8000
-  # - :8000
+		# Debug command of the service
+		# type: string[]
+		# default value: [""]
+		# optional
+        # debug: ["./gradlew", "bootRun", "--debug-jvm"]
 
-# resource requirements of dev container
-# devContainerResources:
-# limits:
-#   memory: 1Gi
-#   cpu: 500m
-# requests:
-#   memory: 100Mi
-#   cpu: 100m
+		# Hot-reload run command of the service
+		# type: string[]
+		# default value: [""]
+		# optional
+        hotReloadRun: ["bash", "-c", "gradlew bootRun"]
 
-# pod selectors which service depends on
-# type: string[]
-# default value: []
-# optional
-# dependPodsLabelSelector:
-  # - "name=mariadb"
-  # - "app.kubernetes.io/name=mariadb"
-
-# job selectors which service depends on
-# type: string[]
-# default value: []
-# optional
-# dependJobsLabelSelector:
-  # - "name=init-job"
-  # - "app.kubernetes.io/name=init-job"
-
-# The default shell of DevContainer
-# type: string
-# default value: "/bin/sh"
-# optional
-# devContainerShell: "bash"
-
-# Build command of the service
-# type: string[]
-# default value: [""]
-# optional
-# buildCommand: ["./gradlew", "package"]
-
-# Run command of the service
-# type: string[]
-# default value: [""]
-# optional
-# runCommand: ["./gradlew", "bootRun"]
-
-# Debug command of the service
-# type: string[]
-# default value: [""]
-# optional
-# debugCommand: ["./gradlew", "bootRun", "--debug-jvm"]
-
-# Hot-reload run command of the service
-# type: string[]
-# default value: [""]
-# optional
-# hotReloadRunCommand: ["bash", "-c", "kill $(ps -ef|grep -i gradlew| grep -v grep| awk '{print $2}'); gradlew bootRun"]
-
-# Hot-reload debug command of the service
-# type: string[]
-# default value: [""]
-# optional
-# hotReloadDebugCommand: ["bash", "-c", "kill $(ps -ef|grep -i gradlew| grep -v grep| awk '{print $2}'); gradlew bootRun --debug-jvm"]
+		# Hot-reload debug command of the service
+		# type: string[]
+		# default value: [""]
+		# optional
+        hotReloadDebug: ["bash", "-c", "gradlew bootRun --debug-jvm"]
+      debug:
+        remoteDebugPort: 5005
+      useDevContainer: false
+      sync:
+        type: send
+		# List of files and directories to be synchronized to DevContainer
+		# type: string[]
+		# default value: ["."]
+		# optional
+        filePattern:
+          - "./src"
+          - "./pkg/fff"
+		# List of ignored files and directories to be synchronized to DevContainer
+		# type: string[]
+		# default value: []
+		# optional
+        ignoreFilePattern:
+          - ".git"
+          - "./build"
+      env:
+      - name: DEBUG
+        value: "true"
+      - name: DOMAIN
+        value: "www.coding.com"
+      envFrom:
+        envFile:
+          - path: dev.env
+          - path: dev.env
+	  # ports which need to be forwarded
+	  # localPort:remotePort
+	  # type: string[]
+	  # default value: []
+	  # optional
+      # portForward:
+      # - 3306:3306
+	  # random localPort, remotePort 8000
+	  # - :8000
 `
 
 func GetSvcTpl(svcName string) (string, error) {
