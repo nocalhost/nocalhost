a4690303b3d05bbee05a719345b0679f605825c9
p_caiwfeng <p_caiwfeng@tencent.com>
2021-05-31 19:11:57 +0800

    add nhctl kexec command

---
+++ cmd/nhctl/cmds/kexec.go
@@ -0,0 +1,54 @@
+/*
+ * Tencent is pleased to support the open source community by making Nocalhost available.,
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under,
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmds
+
+import (
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	logs "k8s.io/kubectl/pkg/cmd/exec"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"nocalhost/pkg/nhctl/clientgoutils"
+	"os"
+	"time"
+)
+
+var options = &logs.ExecOptions{
+	StreamOptions: logs.StreamOptions{
+		IOStreams: genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr},
+	},
+	Executor: &logs.DefaultRemoteExecutor{},
+}
+
+var cmdKexec = &cobra.Command{
+	Use:     "kexec",
+	Example: "kexec (POD | TYPE/NAME) [-c CONTAINER] [flags] -- COMMAND [args...]",
+	Long:    `Execute a command in a container`,
+	Short:   `Execute a command in a container`,
+	Run: func(cmd *cobra.Command, args []string) {
+		clientGoUtils, err := clientgoutils.NewClientGoUtils(kubeConfig, nameSpace)
+		must(err)
+		cmdutil.AddPodRunningTimeoutFlag(cmd, 60*time.Second)
+		cmdutil.AddJsonFilenameFlag(cmd.Flags(), &options.FilenameOptions.Filenames, "to use to exec into the resource")
+		argsLenAtDash := cmd.ArgsLenAtDash()
+		cmdutil.CheckErr(options.Complete(clientGoUtils.NewFactory(), cmd, args, argsLenAtDash))
+		cmdutil.CheckErr(options.Validate())
+		cmdutil.CheckErr(options.Run())
+	},
+}
+
+func init() {
+	cmdKexec.Flags().StringVarP(&options.ContainerName, "container", "c", options.ContainerName, "Container name. If omitted, the first container in the pod will be chosen")
+	cmdKexec.Flags().BoolVarP(&options.Stdin, "stdin", "i", options.Stdin, "Pass stdin to the container")
+	cmdKexec.Flags().BoolVarP(&options.TTY, "tty", "t", options.TTY, "Stdin is a TTY")
+	rootCmd.AddCommand(cmdKexec)
+}
