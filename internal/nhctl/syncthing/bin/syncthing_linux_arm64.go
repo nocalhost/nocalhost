771dd827dd2929cc5659a2539d6624b5cb27399e
p_caiwfeng <p_caiwfeng@tencent.com>
2021-07-23 15:09:45 +0800

    rename .exe suffix

--- internal/nhctl/syncthing/bin/syncthing_linux_arm64.go
+++ internal/nhctl/syncthing/bin/syncthing_linux_arm64.go
@@ -4,21 +4,20 @@ package bin
 
 import (
 	"embed"
-	"io/fs"
 	"io/ioutil"
 )
 
 //go:embed syncthing_linux_arm64
 var f embed.FS
 
-var BinName = "syncthing_linux_arm64"
+var binName = "syncthing_linux_arm64"
 
 func CopyToBinPath(dst string) error {
 	file, err := f.ReadFile(binName)
 	if err != nil {
 		return err
 	}
-	if err = ioutil.WriteFile(dst, file, fs.ModePerm); err != nil {
+	if err = ioutil.WriteFile(dst, file, 0700); err != nil {
 		return err
 	}
 	return nil
--- internal/nhctl/syncthing/bin/syncthing_windows_amd64.go
+++ internal/nhctl/syncthing/bin/syncthing_windows_amd64.go
@@ -4,21 +4,20 @@ package bin
 
 import (
 	"embed"
-	"io/fs"
 	"io/ioutil"
 )
 
-//go:embed syncthing_windows_amd64.exe
+//go:embed syncthing_windows_amd64
 var f embed.FS
 
-var BinName = "syncthing_windows_amd64.exe"
+var binName = "syncthing_windows_amd64"
 
 func CopyToBinPath(dst string) error {
 	file, err := f.ReadFile(binName)
 	if err != nil {
 		return err
 	}
-	if err = ioutil.WriteFile(dst, file, fs.ModePerm); err != nil {
+	if err = ioutil.WriteFile(dst, file, 0700); err != nil {
 		return err
 	}
 	return nil
