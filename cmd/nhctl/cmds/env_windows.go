3a263bff9196f1a900d8301b8e44b2f92881e041
王炜 <434533508@qq.com>
2021-08-02 13:47:41 +0800

    use THL A29 Limited

--- cmd/nhctl/cmds/env_windows.go
+++ cmd/nhctl/cmds/env_windows.go
@@ -1,5 +1,10 @@
 // +build windows
 
+/*
+* Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+* This source code is licensed under the Apache License Version 2.0.
+ */
+
 package cmds
 
 import (
