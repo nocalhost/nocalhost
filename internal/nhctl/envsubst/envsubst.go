3fa74d4d5480b4d0203c07e579c105b419a13a55
anurnomeru <anurnomeru@163.com>
2021-08-25 23:00:13 +0800

    fix(nhctl): fix render with env from

--- internal/nhctl/envsubst/envsubst.go
+++ internal/nhctl/envsubst/envsubst.go
@@ -1,55 +1,23 @@
 /*
 * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
 * This source code is licensed under the Apache License Version 2.0.
-*/
+ */
 
 package envsubst
 
 import (
-	"bufio"
 	"nocalhost/internal/nhctl/envsubst/parse"
 	"nocalhost/internal/nhctl/fp"
-	"nocalhost/pkg/nhctl/log"
 	"os"
-	"strings"
 )
 
 func Render(fp, envFile *fp.FilePathEnhance) (string, error) {
 	envs := [][]string{os.Environ()}
-	envs = append(envs, readEnvFile(envFile)[:])
+	envs = append(envs, envFile.ReadEnvFile()[:])
 
-	return parse.New("string", envs,
-		&parse.Restrictions{NoUnset: false, NoEmpty: false}).
+	return parse.New(
+		"string", envs,
+		&parse.Restrictions{NoUnset: false, NoEmpty: false},
+	).
 		Parse(fp.ReadFile(), fp.Abs(), []string{})
-}
-
-func readEnvFile(envFile *fp.FilePathEnhance) []string {
-	var envFiles []string
-
-	if envFile == nil {
-		return envFiles
-	}
-
-	if err := envFile.CheckExist(); err != nil {
-		return envFiles
-	}
-
-	file, err := os.Open(envFile.Abs())
-	if err != nil {
-		log.ErrorE(err, "Can't not reading env file from "+envFile.Path)
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		text := scanner.Text()
-		if !strings.ContainsAny(text, "=") || strings.HasPrefix(text, "#") {
-			continue
-		}
-		envFiles = append(envFiles, text)
-	}
-
-	return envFiles
 }
