808a6c9a2874d78cf3ad1b3e2974125826cab398
name <name>
2021-09-01 11:45:18 +0800

    feat(nhctl): add timeout to daemon client when reading data from daemon server

--- internal/nhctl/daemon_client/client.go
+++ internal/nhctl/daemon_client/client.go
@@ -19,7 +19,6 @@ import (
 	"nocalhost/internal/nhctl/syncthing/ports"
 	"nocalhost/internal/nhctl/utils"
 	"nocalhost/pkg/nhctl/log"
-	utils2 "nocalhost/pkg/nhctl/utils"
 	"os"
 	"runtime/debug"
 	"time"
@@ -343,13 +342,12 @@ func (d *DaemonClient) SendUpdateApplicationMetaCommand(
 func (d *DaemonClient) sendDataToDaemonServer(data []byte) error {
 	baseCmd := command.BaseCommand{}
 	err := json.Unmarshal(data, &baseCmd)
-	if err == nil {
-		log.Tracef("Sending %v command", baseCmd.CommandType)
+	if err != nil {
+		return errors.Wrap(err, "Failed to unmarshal command")
 	}
 	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", d.daemonServerListenPort), time.Second*30)
 	if err != nil {
-		log.WrapAndLogE(err)
-		return errors.Wrap(err, "")
+		return errors.Wrap(err, fmt.Sprintf("%s failed to dial to daemon", baseCmd.CommandType))
 	}
 	defer conn.Close()
 	if err = conn.SetDeadline(time.Now().Add(time.Second * 30)); err != nil {
@@ -359,23 +357,30 @@ func (d *DaemonClient) sendDataToDaemonServer(data []byte) error {
 		log.Logf("set connection write deadline, err: %v", err)
 	}
 	_, err = conn.Write(data)
-	log.WrapAndLogE(err)
-	return errors.Wrap(err, "")
+	return errors.Wrap(err, fmt.Sprintf("%s failed to write to daemon", baseCmd.CommandType))
 }
 
 // sendAndWaitForResponse send data to daemon and wait for response
 func (d *DaemonClient) sendAndWaitForResponse(req []byte, resp interface{}) error {
+	var (
+		conn net.Conn
+		err  error
+	)
+	defer func() {
+		if conn != nil {
+			conn.Close()
+		}
+	}()
+
 	baseCmd := command.BaseCommand{}
-	err := json.Unmarshal(req, &baseCmd)
-	if err == nil {
-		log.Tracef("Sending %v command", baseCmd.CommandType)
-	}
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", d.daemonServerListenPort), time.Second*30)
+	err = json.Unmarshal(req, &baseCmd)
 	if err != nil {
-		log.WrapAndLogE(err)
-		return utils2.WrapErr(err)
+		return errors.Wrap(err, "Failed to unmarshal command")
 	}
-	defer conn.Close()
+	conn, err = net.DialTimeout("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", d.daemonServerListenPort), time.Second*30)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("%s failed to dial to daemon", baseCmd.CommandType))
+	}
 
 	//if err = conn.SetDeadline(time.Now().Add(time.Second * 30)); err != nil {
 	//	log.Logf("set connection deadline, err: %v", err)
@@ -388,26 +393,39 @@ func (d *DaemonClient) sendAndWaitForResponse(req []byte, resp interface{}) erro
 	//}
 
 	if _, err = conn.Write(req); err != nil {
-		log.WrapAndLogE(err)
-		return utils2.WrapErr(err)
+		return errors.Wrap(err, fmt.Sprintf("%s failed to write to daemon", baseCmd.CommandType))
 	}
 	cw, ok := conn.(interface{ CloseWrite() error })
 	if !ok {
-		err = errors.New("Error to close write to daemon server")
-		log.LogE(err)
-		return err
+		return errors.New(fmt.Sprintf("%s failed to close write to daemon server", baseCmd.CommandType))
 	}
 	log.WrapAndLogE(cw.CloseWrite())
 
-	respBytes, err := ioutil.ReadAll(conn)
-	if err != nil {
-		log.WrapAndLogE(err)
-		return errors.Wrap(err, "")
+	errChan := make(chan error, 0)
+	var respBytes []byte
+
+	go func() {
+		var err error
+		respBytes, err = ioutil.ReadAll(conn)
+		errChan <- err
+	}()
+
+	select {
+	case err = <-errChan:
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("%s failed to get response from daemon", baseCmd.CommandType))
+		}
+	case <-time.After(30 * time.Second):
+		return errors.New(fmt.Sprintf("%s waited response timeout after 30s", baseCmd.CommandType))
+
+	}
+
+	if len(respBytes) == 0 {
+		return errors.New(fmt.Sprintf("Daemon responses empty data to %s", baseCmd.CommandType))
 	}
 
 	response := command.BaseResponse{}
 	if err := json.Unmarshal(respBytes, &response); err != nil {
-		log.WrapAndLogE(err)
 		return errors.Wrap(err, "")
 	}
 
@@ -427,7 +445,6 @@ func (d *DaemonClient) sendAndWaitForResponse(req []byte, resp interface{}) erro
 		err = errors.New(
 			fmt.Sprintf("Error occur from daemon, status [%d], msg [%s].", response.Status, response.Msg),
 		)
-		log.LogE(err)
 		return err
 	}
 
@@ -439,8 +456,7 @@ func (d *DaemonClient) sendAndWaitForResponse(req []byte, resp interface{}) erro
 		return nil
 	}
 	if err := json.Unmarshal(response.Data, resp); err != nil {
-		log.WrapAndLogE(err)
-		return errors.Wrap(err, "")
+		return errors.Wrap(err, fmt.Sprintf("%s failed to unmarshal response data", baseCmd.CommandType))
 	}
 
 	return nil
