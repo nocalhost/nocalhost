469c597420ca4f32233028477d8b024b95b6c8aa
p_caiwfeng <p_caiwfeng@tencent.com>
2021-09-01 17:06:06 +0800

    support all kinds of type resource to exec command

--- internal/nhctl/app/exec.go
+++ internal/nhctl/app/exec.go
@@ -6,33 +6,30 @@
 package app
 
 import (
-	"fmt"
-	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
-	"nocalhost/pkg/nhctl/log"
 )
 
-func (a *Application) Exec(svcName string, container string, commands []string) error {
-	podList, err := a.client.ListPodsByDeployment(svcName)
-	if err != nil {
-		return err
-	}
-	var runningPod = make([]v1.Pod, 0, 1)
-	for _, item := range podList.Items {
-		if item.Status.Phase == v1.PodRunning && item.DeletionTimestamp == nil {
-			runningPod = append(runningPod, item)
-		}
-	}
-	if len(runningPod) != 1 {
-		log.Warnf("the number of pods of %s is not 1 ???", svcName)
-		return errors.New(fmt.Sprintf("the number of pods of %s is not 1 ???", svcName))
-	}
-	pod := runningPod[0].Name
+func (a *Application) Exec(pod v1.Pod, container string, commands []string) error {
+	//podList, err := a.client.ListPodsByDeployment(svcName)
+	//if err != nil {
+	//	return err
+	//}
+	//var runningPod = make([]v1.Pod, 0, 1)
+	//for _, item := range podList.Items {
+	//	if item.Status.Phase == v1.PodRunning && item.DeletionTimestamp == nil {
+	//		runningPod = append(runningPod, item)
+	//	}
+	//}
+	//if len(runningPod) != 1 {
+	//	log.Warnf("the number of pods of %s is not 1 ???", svcName)
+	//	return errors.New(fmt.Sprintf("the number of pods of %s is not 1 ???", svcName))
+	//}
+	//pod := runningPod[0].Name
 	var name string
 	// if container arguments are available, using container arguments
 	// else if found nocalhost-dev container, using nocalhost-dev
 	// else return error
-	for _, c := range runningPod[0].Spec.Containers {
+	for _, c := range pod.Spec.Containers {
 		if c.Name == "nocalhost-dev" {
 			name = c.Name
 		}
@@ -41,5 +38,5 @@ func (a *Application) Exec(svcName string, container string, commands []string) 
 			break
 		}
 	}
-	return a.client.Exec(pod, name, commands)
+	return a.client.Exec(pod.Name, name, commands)
 }
