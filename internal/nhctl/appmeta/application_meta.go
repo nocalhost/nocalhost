c264c77ef806f000a396cbc8e01d494a53970135
anurnomeru <anurnomeru@163.com>
2021-09-28 15:05:40 +0800

    feat(nhctl): fix secret updatation while dev start

--- internal/nhctl/appmeta/application_meta.go
+++ internal/nhctl/appmeta/application_meta.go
@@ -570,7 +570,15 @@ func (a *ApplicationMeta) CheckIfSvcDeveloping(name string, svcType base.SvcType
 func (a *ApplicationMeta) Update() error {
 	return retry.OnError(
 		retry.DefaultRetry, func(err error) bool {
-			return err != nil
+			if err != nil {
+				if secret, err := a.operator.ReObtainSecret(a.Ns, a.Secret); err != nil {
+					return false
+				} else {
+					a.Secret = secret
+					return true
+				}
+			}
+			return false
 		}, func() error {
 			a.prepare()
 			secret, err := a.operator.Update(a.Ns, a.Secret)
