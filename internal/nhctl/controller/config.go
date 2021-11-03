fed16f53ef401c6f0c3bcb97ce89c29a0e2b3ce2
name <name>
2021-11-03 11:59:56 +0800

    fix(nhctl): fix config ui err

--- internal/nhctl/controller/config.go
+++ internal/nhctl/controller/config.go
@@ -19,6 +19,12 @@ func (c *Controller) Config() *profile.ServiceConfigV2 {
 	return c.config
 }
 
+func (c *Controller) ReloadConfig() profile.ServiceConfigV2 {
+	config := c.AppMeta.Config.GetSvcConfigS(c.Name, c.Type)
+	c.config = &config
+	return config
+}
+
 func (c *Controller) GetAppConfig() *profile.NocalHostAppConfigV2 {
 	return c.AppMeta.Config
 }
