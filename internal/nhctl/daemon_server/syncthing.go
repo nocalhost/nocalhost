57fdc82e67b9df42c65bd1b7461961b46414fec9
p_caiwfeng <p_caiwfeng@tencent.com>
2021-11-11 19:14:16 +0800

    backoff recover syncthing

--- internal/nhctl/daemon_server/syncthing.go
+++ internal/nhctl/daemon_server/syncthing.go
@@ -179,12 +179,12 @@ func reconnectedSyncthingIfNeeded() {
 						v.(*name).lastTime = time.Now()
 						v.(*name).times++
 						if v.(*name).times >= 5 {
-							v.(*name).nextTime = time.Now().Add(time.Minute * 2)
+							v.(*name).nextTime = time.Now().Add(time.Minute * 3)
 						}
 					} else {
 						maps.Store(toKey(svc), &name{times: 1, lastTime: time.Now()})
 					}
-					// if last 5 min failed 5 times, will not retry
+					// if last 5 min failed 5 times, will delay 3 min to retry
 					if v, ok := maps.Load(toKey(svc)); ok {
 						if time.Now().Sub(v.(*name).lastTime).Minutes() >= 5 && v.(*name).times >= 5 {
 							if v.(*name).nextTime.Sub(time.Now()).Seconds() > 0 {
