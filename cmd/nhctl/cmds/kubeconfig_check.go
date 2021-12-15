291c5b3de52807becdbe04d3dc4427a430b62fa4
anurnomeru <anurnomeru@163.com>
2021-12-15 11:10:28 +0800

    feat: optimize for kubeconfig check

--- cmd/nhctl/cmds/kubeconfig_check.go
+++ cmd/nhctl/cmds/kubeconfig_check.go
@@ -23,12 +23,15 @@ var (
 
 func init() {
 	kubeconfigCmd.AddCommand(kubeconfigCheckCmd)
-	kubeconfigCheckCmd.Flags().StringArrayVarP(&contextSpecified,
+	kubeconfigCheckCmd.Flags().StringArrayVarP(
+		&contextSpecified,
 		"context", "c", []string{},
 		"By default, the current context is used. If there is no cluster scope permission, check the 'namespace' is specified",
 	)
-	kubeconfigCheckCmd.Flags().BoolVarP(&interactive,
-		"interactive", "i", false, "return readable interactive result")
+	kubeconfigCheckCmd.Flags().BoolVarP(
+		&interactive,
+		"interactive", "i", false, "return readable interactive result",
+	)
 }
 
 // Check kubeconfig file, if the kubeconfig does have a 'ns list' permission, return nothing
@@ -52,9 +55,9 @@ var kubeconfigCheckCmd = &cobra.Command{
 			checkInfo := CheckKubeconfig(kubeConfig, context)
 			checkInfos = append(checkInfos, checkInfo)
 
-			if checkInfo.Hint != "" {
+			if checkInfo.Tips != "" {
 				notEmpty = true
-				warnMsg += fmt.Sprintf("%s <br>", checkInfo.Hint)
+				warnMsg += fmt.Sprintf("%s <br>", checkInfo.Tips)
 			}
 		}
 
@@ -73,25 +76,26 @@ var kubeconfigCheckCmd = &cobra.Command{
 // first is the complate guide
 // second is a simple msg
 func CheckKubeconfig(kubeconfigParams string, contextParam string) CheckInfo {
+	unexpectedErrFmt := "Please check if your cluster or kubeconfig is valid, Error: %v"
+	invalidNamespaceFmt := "Please check current namespace is valid, or make sure" +
+		" you have the correct permissions to access this namespace, Error: %v"
+
 	utils, err := clientgoutils.NewClientGoUtils(kubeconfigParams, "")
 	if err != nil {
 		return CheckInfo{
-			FAIL, InfoInvalid, err.Error(), true, err.Error(),
+			FAIL, fmt.Sprintf(unexpectedErrFmt, err.Error()),
 		}
 	}
 
 	config, err := utils.NewFactory().ToRawKubeConfigLoader().RawConfig()
 	if err != nil {
 		return CheckInfo{
-			FAIL, InfoInvalid, err.Error(), true, err.Error(),
+			FAIL, fmt.Sprintf(unexpectedErrFmt, err.Error()),
 		}
 	}
 
 	if len(config.Contexts) == 0 {
-		msg := "Please make sure your kubeconfig contains one context at least "
-		return CheckInfo{
-			FAIL, InfoInvalid, msg, true, msg,
-		}
+		return CheckInfo{FAIL, "Please make sure your kubeconfig contains at least one context"}
 	}
 
 	if contextParam == "" {
@@ -110,16 +114,18 @@ func CheckKubeconfig(kubeconfigParams string, contextParam string) CheckInfo {
 			contextParam, set.UnsortedList(),
 		)
 		return CheckInfo{
-			FAIL, InfoInvalid, msg, true, msg,
+			FAIL, msg,
 		}
 	} else {
 		specifiedNs := ctx.Namespace == ""
 		_ = Prepare()
 
+		kubeCOnfigContent := fp.NewFilePath(kubeConfig).ReadFile()
+
 		err := clientgoutils.CheckForResource(
-			fp.NewFilePath(kubeConfig).ReadFile(),
+			kubeCOnfigContent,
 			"",
-			[]string{"list", "get", "watch"}, "", "namespaces",
+			[]string{"list", "get", "watch"}, "namespaces",
 		)
 
 		if err != nil {
@@ -132,41 +138,34 @@ func CheckKubeconfig(kubeconfigParams string, contextParam string) CheckInfo {
 				)
 
 				if specifiedNs {
-					return CheckInfo{
-						SUCCESS, ctx.Namespace, fmt.Sprintf("Current context can list the resources behind ns %s", ctx.Namespace),
-						true, "",
+					if err := clientgoutils.CheckForResource(
+						kubeCOnfigContent,
+						"",
+						[]string{"list"}, "pod",
+					); err != nil {
+						return CheckInfo{FAIL, fmt.Sprintf(invalidNamespaceFmt, err.Error())}
 					}
+					return CheckInfo{SUCCESS, ""}
 				}
 
-				return CheckInfo{
-					FAIL, "Type in a namespace", "Please Type in a correct namespace and try again", true, msg,
-				}
+				return CheckInfo{FAIL, msg}
 			}
-			return CheckInfo{
-				FAIL, "Type in a namespace", "Please Type in a correct namespace and try again", true, err.Error(),
-			}
+			return CheckInfo{FAIL, fmt.Sprintf(unexpectedErrFmt, err.Error())}
 		}
 
 		// Success with cluster scope
-		return CheckInfo{
-			SUCCESS, "Cluster-Scope", "Current context can list with cluster-scope resources", false, "",
-		}
+		return CheckInfo{SUCCESS, "Current context can list with cluster-scope resources"}
 	}
 }
 
 var (
 	SUCCESS CheckInfoStatus = "SUCCESS"
 	FAIL    CheckInfoStatus = "FAIL"
-
-	InfoInvalid = "invalid"
 )
 
 type CheckInfo struct {
 	Status CheckInfoStatus `json:"status" yaml:"status"`
-	Info   string          `json:"info" yaml:"info"`
 	Tips   string          `json:"tips" yaml:"tips"`
-	TypeIn bool            `json:"typein" yaml:"typein"`
-	Hint   string          `json:"hint" yaml:"hint"`
 }
 
 type CheckInfoStatus string
