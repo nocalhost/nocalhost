b0082fba1dc96d6011539a934d31e08c89431d35
p_caiwfeng <p_caiwfeng@tencent.com>
2022-01-04 17:16:22 +0800

    add thanks for gost

--- internal/nhctl/vpn/core/tunhandler.go
+++ internal/nhctl/vpn/core/tunhandler.go
@@ -2,6 +2,7 @@
 * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
 * This source code is licensed under the Apache License Version 2.0.
  */
+// thanks to project gost for given our inspired
 
 package core
 
