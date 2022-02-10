ce094649c8fcd5f3a4d6425d507ec25d945dce17
anurnomeru <anurnomeru@163.com>
2022-02-10 16:42:28 +0800

    feat(nhctl): fix exec pod when first enter devmode

--- cmd/nhctl/cmds/exec.go
+++ cmd/nhctl/cmds/exec.go
@@ -10,6 +10,7 @@ import (
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
 	"nocalhost/cmd/nhctl/cmds/common"
+	_const "nocalhost/internal/nhctl/const"
 	"nocalhost/pkg/nhctl/log"
 	"regexp"
 )
@@ -23,13 +24,19 @@ type ExecFlags struct {
 var execFlags = ExecFlags{}
 
 func init() {
-	execCmd.Flags().StringArrayVarP(&execFlags.Commands, "command", "c", nil,
-		"command to execute in container")
+	execCmd.Flags().StringArrayVarP(
+		&execFlags.Commands, "command", "c", nil,
+		"command to execute in container",
+	)
 	execCmd.Flags().StringVarP(&execFlags.Container, "container", "", "", "container name")
-	execCmd.Flags().StringVarP(&execFlags.SvcName, "deployment", "d", "",
-		"k8s deployment which your developing service exists")
-	execCmd.Flags().StringVarP(&common.ServiceType, "controller-type", "t", "deployment",
-		"kind of k8s controller,such as deployment,statefulSet")
+	execCmd.Flags().StringVarP(
+		&execFlags.SvcName, "deployment", "d", "",
+		"k8s deployment which your developing service exists",
+	)
+	execCmd.Flags().StringVarP(
+		&common.ServiceType, "controller-type", "t", "deployment",
+		"kind of k8s controller,such as deployment,statefulSet",
+	)
 	rootCmd.AddCommand(execCmd)
 }
 
@@ -56,14 +63,26 @@ var execCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 		runningPod := make([]v1.Pod, 0, 1)
+
+		var ableToExec *v1.Pod = nil
+
+	Exit:
 		for _, item := range podList {
 			if item.Status.Phase == v1.PodRunning && item.DeletionTimestamp == nil {
 				runningPod = append(runningPod, item)
 			}
+
+			for _, c := range item.Spec.Containers {
+				if c.Name == _const.NocalhostDefaultDevSidecarName {
+					ableToExec = &item
+					break Exit
+				}
+			}
 		}
-		if len(runningPod) != 1 {
+		if ableToExec == nil && len(runningPod) != 1 {
 			log.Fatalf("pod number: %d, is not 1, please make sure pod number is 1", len(runningPod))
 		}
-		must(common.NocalhostApp.Exec(runningPod[0], execFlags.Container, execFlags.Commands))
+		ableToExec = &runningPod[0]
+		must(common.NocalhostApp.Exec(*ableToExec, execFlags.Container, execFlags.Commands))
 	},
 }
