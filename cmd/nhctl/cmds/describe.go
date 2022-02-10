01d9f895f0eebac3420f759eb1f51fdee7246d68
name <name>
2022-02-10 11:27:39 +0800

    feat(nhctl): remove describe cmd

--- cmd/nhctl/cmds/describe.go
+++ cmd/nhctl/cmds/describe.go
@@ -5,54 +5,55 @@
 
 package cmds
 
-import (
-	"fmt"
-	"github.com/pkg/errors"
-	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
-	"nocalhost/cmd/nhctl/cmds/common"
-	"nocalhost/internal/nhctl/common/base"
-)
-
-var deploy string
-
-func init() {
-	describeCmd.Flags().StringVarP(&deploy, "deployment", "d", "",
-		"k8s deployment which your developing service exists",
-	)
-	describeCmd.Flags().StringVarP(&common.ServiceType, "type", "t", "deployment", "specify service type")
-	rootCmd.AddCommand(describeCmd)
-}
-
-var describeCmd = &cobra.Command{
-	Use:   "describe [NAME]",
-	Short: "Describe application info",
-	Long:  `Describe application info`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.Errorf("%q requires at least 1 argument\n", cmd.CommandPath())
-		}
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		applicationName := args[0]
-		common.InitApp(applicationName)
-		if deploy == "" {
-			appProfile := common.NocalhostApp.GetDescription()
-			for _, svcProfileV2 := range appProfile.SvcProfile {
-				svcProfileV2.DevModeType = common.NocalhostApp.GetAppMeta().GetCurrentDevModeTypeOfWorkload(svcProfileV2.Name, base.SvcType(svcProfileV2.Type), appProfile.Identifier)
-			}
-			bytes, err := yaml.Marshal(appProfile)
-			if err == nil {
-				fmt.Print(string(bytes))
-			}
-		} else {
-			common.CheckIfSvcExist(deploy, common.ServiceType)
-			svcProfile := common.NocalhostSvc.GetDescription()
-			bytes, err := yaml.Marshal(svcProfile)
-			if err == nil {
-				fmt.Print(string(bytes))
-			}
-		}
-	},
-}
+//
+//import (
+//	"fmt"
+//	"github.com/pkg/errors"
+//	"github.com/spf13/cobra"
+//	"gopkg.in/yaml.v3"
+//	"nocalhost/cmd/nhctl/cmds/common"
+//	"nocalhost/internal/nhctl/common/base"
+//)
+//
+//var deploy string
+//
+//func init() {
+//	describeCmd.Flags().StringVarP(&deploy, "deployment", "d", "",
+//		"k8s deployment which your developing service exists",
+//	)
+//	describeCmd.Flags().StringVarP(&common.ServiceType, "type", "t", "deployment", "specify service type")
+//	rootCmd.AddCommand(describeCmd)
+//}
+//
+//var describeCmd = &cobra.Command{
+//	Use:   "describe [NAME]",
+//	Short: "Describe application info",
+//	Long:  `Describe application info`,
+//	Args: func(cmd *cobra.Command, args []string) error {
+//		if len(args) < 1 {
+//			return errors.Errorf("%q requires at least 1 argument\n", cmd.CommandPath())
+//		}
+//		return nil
+//	},
+//	Run: func(cmd *cobra.Command, args []string) {
+//		applicationName := args[0]
+//		common.InitApp(applicationName)
+//		if deploy == "" {
+//			appProfile := common.NocalhostApp.GetDescription()
+//			for _, svcProfileV2 := range appProfile.SvcProfile {
+//				svcProfileV2.DevModeType = common.NocalhostApp.GetAppMeta().GetCurrentDevModeTypeOfWorkload(svcProfileV2.Name, base.SvcType(svcProfileV2.Type), appProfile.Identifier)
+//			}
+//			bytes, err := yaml.Marshal(appProfile)
+//			if err == nil {
+//				fmt.Print(string(bytes))
+//			}
+//		} else {
+//			common.CheckIfSvcExist(deploy, common.ServiceType)
+//			svcProfile := common.NocalhostSvc.GetDescription()
+//			bytes, err := yaml.Marshal(svcProfile)
+//			if err == nil {
+//				fmt.Print(string(bytes))
+//			}
+//		}
+//	},
+//}
