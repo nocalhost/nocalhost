a5f578a358676ed24fb1c63606f59f3c3284b9a7
p_caiwfeng <p_caiwfeng@tencent.com>
2022-02-18 10:50:58 +0800

    feat(nhctl): support port-forward
    Signed-off-by: p_caiwfeng <p_caiwfeng@tencent.com>

--- internal/nhctl/vpn/pkg/handler/controller.go
+++ internal/nhctl/vpn/pkg/handler/controller.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	_const "nocalhost/internal/nhctl/const"
 	"strconv"
 	"strings"
 )
@@ -41,18 +42,18 @@ func AddContainer(spec *v1.PodSpec, c *PodRouteConfig) {
 	zero := int64(0)
 	spec.Containers = append(spec.Containers, v1.Container{
 		Name:    VPN,
-		Image:   "naison/kubevpn:v2",
+		Image:   _const.DefaultVPNImage,
 		Command: []string{"/bin/sh", "-c"},
 		Args: []string{
 			"sysctl net.ipv4.ip_forward=1;" +
 				"iptables -F;" +
 				"iptables -P INPUT ACCEPT;" +
 				"iptables -P FORWARD ACCEPT;" +
-				"iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80:60000 -j DNAT --to " + c.LocalTunIP + ":80-60000;" +
-				"iptables -t nat -A POSTROUTING -p tcp -m tcp --dport 80:60000 -j MASQUERADE;" +
-				"iptables -t nat -A PREROUTING -i eth0 -p udp --dport 80:60000 -j DNAT --to " + c.LocalTunIP + ":80-60000;" +
-				"iptables -t nat -A POSTROUTING -p udp -m udp --dport 80:60000 -j MASQUERADE;" +
-				"kubevpn serve -L 'tun://0.0.0.0:8421/" + c.TrafficManagerRealIP + ":8421?net=" + c.InboundPodTunIP + "&route=" + c.Route + "' --debug=true",
+				"iptables -t nat -A PREROUTING ! -p icmp -j DNAT --to " + c.LocalTunIP + ";" +
+				"iptables -t nat -A POSTROUTING ! -p icmp -j MASQUERADE;" +
+				"sysctl -w net.ipv4.conf.all.route_localnet=1;" +
+				"iptables -t nat -A OUTPUT -o lo ! -p icmp -j DNAT --to-destination " + c.LocalTunIP + ";" +
+				"nhctl vpn serve -L 'tun://0.0.0.0:8421/" + c.TrafficManagerRealIP + ":8421?net=" + c.InboundPodTunIP + "&route=" + c.Route + "' --debug=true",
 		},
 		SecurityContext: &v1.SecurityContext{
 			Capabilities: &v1.Capabilities{
