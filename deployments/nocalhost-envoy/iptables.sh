d30ed8d32d152cc928a4293a711d65cedac232ea
wencaiwulue <33818115+wencaiwulue@users.noreply.github.com>
2022-04-08 15:02:05 +0800

    mesh mode only works on container-port (#1283)
    Signed-off-by: p_caiwfeng <p_caiwfeng@tencent.com>
    
    Co-authored-by: p_caiwfeng <p_caiwfeng@tencent.com>

--- deployments/nocalhost-envoy/iptables.sh
+++ deployments/nocalhost-envoy/iptables.sh
@@ -10,7 +10,7 @@ iptables -t nat -X NOCALHOST_REDIRECT 2>/dev/null
 
 set -e
 iptables -t nat -N NOCALHOST_REDIRECT
-iptables -t nat -A NOCALHOST_REDIRECT -p tcp -j REDIRECT --to-port 16006
+iptables -t nat -A NOCALHOST_REDIRECT -p tcp -m multiport --dport "${NOCALHOST_PORT}" -j REDIRECT --to-port 16006
 iptables -t nat -N NOCALHOST_INBOUND
-iptables -t nat -A NOCALHOST_INBOUND -p tcp  -j NOCALHOST_REDIRECT
+iptables -t nat -A NOCALHOST_INBOUND -p tcp -j NOCALHOST_REDIRECT
 iptables -t nat -A PREROUTING -p tcp -j NOCALHOST_INBOUND
