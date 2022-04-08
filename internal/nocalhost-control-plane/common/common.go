c8c0ea981606a3488ec5bec45e54667917ab7158
p_caiwfeng <p_caiwfeng@tencent.com>
2022-04-08 10:51:59 +0800

    mesh mode only works on container-port
    Signed-off-by: p_caiwfeng <p_caiwfeng@tencent.com>

--- internal/nocalhost-control-plane/common/common.go
+++ internal/nocalhost-control-plane/common/common.go
@@ -1,16 +1,18 @@
 package common
 
+import "nocalhost/internal/nhctl/controller"
+
 const (
 	ListenerAddress    = "0.0.0.0"
 	PassthroughCluster = "PassthroughCluster"
 )
 
 const (
-	MeshKey        = "dev.mesh.nocalhost.dev"
-	MeshUUIDKEY    = "dev.mesh.nocalhost.dev/uuid"
-	MeshTypeKEY    = "dev.mesh.nocalhost.dev/type"
-	MeshHeaderKey  = "dev.mesh.nocalhost.dev/header-key"
-	MeshHeaderVal  = "dev.mesh.nocalhost.dev/header-value"
-	MeshDevType    = "dev"
-	MeshOriginType = "origin"
+	MeshKey        = controller.AnnotationMeshEnable
+	MeshUUIDKEY    = controller.AnnotationMeshUuid
+	MeshTypeKEY    = controller.AnnotationMeshType
+	MeshHeaderKey  = controller.AnnotationMeshHeaderKey
+	MeshHeaderVal  = controller.AnnotationMeshHeaderValue
+	MeshDevType    = controller.AnnotationMeshTypeDev
+	MeshOriginType = controller.AnnotationMeshTypeOrigin
 )
